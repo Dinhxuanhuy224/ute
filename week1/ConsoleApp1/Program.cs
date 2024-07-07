39674a00517c6b47454567d10e57e05491b46d79
xuanhuy <dinhxuanhuy.pro@gmail.com>
2024-07-07 22:24:53 +0700

    moithem folder

---
+++ week1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace xuanhuy
+{
+    class tuan1
+    {
+        static void Main()
+        {
+            int n1 = 4;
+            int n2 = 3;
+            Console.WriteLine(n1+n2);
+        }
+    }
+
+
+
+
+}
