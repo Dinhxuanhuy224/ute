52f29b1297d1c79f0cc9fe8dd681f978a8b823ba
xuanhuy <dinhxuanhuy.pro@gmail.com>
2024-07-07 22:25:57 +0700

    them toan bo

--- Hello_world/ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ Hello_world/ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+719eb8e28fae3296460ad8b229dd0086e6637015")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
